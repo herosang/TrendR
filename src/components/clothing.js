7b6af14edb265752de8eb05af4438c55409a33b8
KevinMKang <kevinmkang97@gmail.com>
2017-01-08 23:04:19 -0500

    Revert "simple class for a button, gonna write this at school"
    This reverts commit 7489617fc3a77e51af6535106f3217ef307ea071.

--- src/components/clothing.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Buttom from 'material-ui/Button';
-
-/* Class for a simple button that lets you attach a picture of clothing on it with a link to its reference through props/parent intereaction */
-
-export default class Clothing extends React.Component{
-	//4 hours of sleep i'll write this tomorrow
-}
