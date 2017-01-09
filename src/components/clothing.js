6a4f810ed5b206dac542adbb06c2345e7626e61a
KevinMKang <kevinmkang97@gmail.com>
2017-01-08 23:01:30 -0500

    simple class for a button, gonna write this at school

---
+++ src/components/clothing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Buttom from 'material-ui/Button';
+
+/* Class for a simple button that lets you attach a picture of clothing on it with a link to its reference through props/parent intereaction */
+
+export default class Clothing extends React.Component{
+	//4 hours of sleep i'll write this tomorrow
+}
