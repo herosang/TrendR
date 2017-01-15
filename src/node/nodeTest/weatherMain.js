df7ba1994563be2c13949b764cba909107bf5447
Hongyu Wang <hongyu.wang@mail.utoronto.ca>
2017-01-15 18:47:44 -0500

    package

--- src/node/nodeTest/weatherMain.js
+++ src/node/nodeTest/weatherMain.js
@@ -1,12 +1,11 @@
 var weather = require("./text.json");
 
-	
+
 
 
 function degrees(num){
 	var val = Math.floor((num/22.5)+.5)
-    var arr=[
-    	"N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    var arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
     return arr[val%16];
 }
 
