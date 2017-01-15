18284e7caed3ed4f32bd3a7f9de1a1a082108b40
darwin.truong@outlook.com <darwin.truong@outlook.com>
2017-01-14 23:40:18 -0500

    fixed that reloading bug

--- src/app.js
+++ src/app.js
@@ -3,22 +3,32 @@ import styles from './css/index.scss';
 import RecoDisplay from './components/recoDisplay.js';
 import First from './components/first.js';
 import Result from './components/result.js';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export default class App extends React.Component {
 
   displayName: 'App';
   props: any;
+
   constructor(props){
     super(props);
     this.state = { };
   }
 
+  getChildContext() {
+    return {muiTheme: getMuiTheme()};
+  };
+
   //display is just there for test
   render () {
     return (
-      <div className = {styles.main}>
-        <RecoDisplay />
+      <div /**className = {styles.main}**/>
+        <Result />
       </div>
     );
   }
 }
+
+App.childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+};
